b66703e9ba8ac7a0bd2ec9e9e98f1869e264085a
RICHARD F PETERS III <rickp_g31@gmail.com>
2016-09-07 09:48:09 -0600

    :sparkles

---
+++ seeds/artist.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('artist').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('artist').insert({id: 1, name: 'The Beatnuts'}),
+        knex('artist').insert({id: 2, name: 'The Flaming Lips'})
+
+      ]);
+    });
+};
